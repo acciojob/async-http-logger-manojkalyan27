dc4af530c3453dbbd70bd30dd25b03b3640b9dcd
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2024-11-25 16:36:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+.then((res)=>res.json())
+.then((res)=>{
+	console.log(res);
+})
+.catch((error) => {
+    console.error("Error fetching the API:", error); // Handle errors
+  });
